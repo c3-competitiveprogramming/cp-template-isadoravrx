86d3d986d5d99ca1dee47909adb83b3ffd9008e6
isadoravrx <isavitoriaxavier@gmail.com>
2023-05-06 11:23:07 -0300

    competição chocolate done

--- spoj/competicaochocolate.cpp
+++ spoj/competicaochocolate.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n,m;
+int main()
+{
+    int n, m;
     cin >> n >> m;
 
-    if(n % 2 != 0 && m % 2 != 0){
+    int plusone = m + 1;
+
+    if ((n > m + 1 && n <= (2 * m + 1)) || m > n){
         cout << "Paula" << endl;
-    }else if(n % 2 != 0 && m % 2 == 0){
-        cout << "Carlos" << endl;
-    }else if(n % 2 == 0 && m % 2 == 0){
-        cout << "Paula" << endl;
-    }else if(n % 2 == 0 && m % 2 != 0){
-        cout << "Carlos" << endl;
     }
+    else{
+        if (n % (m + 1) == 0){
+            cout << "Carlos" << endl;
+        }else{
+            cout << "Paula" << endl;
+        }
+    }
+
+    return 0;
 }
+      
