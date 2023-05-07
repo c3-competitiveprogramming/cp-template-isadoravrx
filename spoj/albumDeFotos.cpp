d19931d7ddb971dc0ad8e0a46ec1f8b22f9ac55a
isadoravrx <isavitoriaxavier@gmail.com>
2023-05-07 11:48:15 -0300

    identificando se o número é primo(selos) done

--- spoj/albumDeFotos.cpp
+++ spoj/albumDeFotos.cpp
@@ -5,12 +5,8 @@ int main(){
     int xpg,ypg;
     int x1,y1,x2,y2;
     cin >> xpg >> ypg >> x1 >> y1 >> x2 >> y2;
-    int areapag = xpg * ypg;
-    int areafotos = (x1 * y1) + (x2 * y2);
-    if(areapag >= areafotos && (x1 + x2 <= xpg && y1 + y2 <= ypg)){
-        cout << "S" << endl;
-    }else{
-        cout << "N" << endl;
+    if(){
+
     }
 
     return 0;
