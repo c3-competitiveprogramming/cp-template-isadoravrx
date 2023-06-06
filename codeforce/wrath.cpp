dc5de9404a30102e986ac79e603db4cddf3dadf4
isadoravrx <isavitoriaxavier@gmail.com>
2023-06-06 15:07:07 -0300

    wrath com tle

---
+++ codeforce/wrath.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int>pessoas;
+    int value;
+    for(int i = 0; i < n; i++){
+        cin >> value;
+        pessoas.push_back(value);
+    }
+
+    for(int i = 0; i < pessoas.size(); i++){
+        for(int j = 0; j < pessoas.size(); j++){
+            if(i == j){
+                continue;
+            }
+
+            if(pessoas[j] == -1){
+                continue;
+            }
+
+            if(i > j && j >= i - pessoas[i]){
+                pessoas[j] = -1;
+            }
+        }
+    }
+
+    int alives = 0;
+    for(int i = 0; i < pessoas.size(); i++){
+        if(pessoas[i] != -1){
+            alives++;
+        }
+    }
+
+    cout << alives << endl;
+}
