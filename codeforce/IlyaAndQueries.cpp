27a0bc1a47d0564cea925ead382ab52c3d19713a
isadoravrx <isavitoriaxavier@gmail.com>
2023-07-03 15:16:37 -0300

    just waiting codeforce return, to send ilya and queries

--- codeforce/IlyaAndQueries.cpp
+++ codeforce/IlyaAndQueries.cpp
@@ -6,26 +6,46 @@ using namespace std;
 int main(){
 
     string caracters;
-    int consultas;
-    cin >> caracters >> consultas;
+    cin >> caracters;
 
     vector<int>nextIsEqual;
 
-    int nowE;
-    nowE = 0; 
-        while(true){
-            if(nowD == indexD){ //significa que literalmente o nowe está no nowD considerando que começamos a contar do 1
-                break;
-            }
-
-            if(caracters[nowE] == caracters[nowD]){
-                counter++;
-            }
-            nowE++;
-            nowD++;
+    int nowE = 0; 
+    
+    while(true){
+        if(nowE == caracters.size() - 1){ //nao podemos olhar o prox do ultimo
+            nextIsEqual.push_back(0);
+            break;
         }
 
+        if(caracters[nowE] == caracters[nowE + 1]){
+            nextIsEqual.push_back(1);
+        }else{
+            nextIsEqual.push_back(0);
+        }
+        
+        nowE++;
+         
+    }
 
+    int queries;
+    cin >> queries;
+
+    while(queries--){ //lembrando que eu nao posso olhar o proximo do d, eu vou até d-1
+        int e,d;
+        cin >> e >> d;
+        e--;
+        d--;
+        int sum = 0;
+        int point = e;
+        while(point != d){
+            sum += nextIsEqual[point];
+            point++;
+        }
+
+        cout << sum << endl;
+
+    }
 
     return 0;
 }
