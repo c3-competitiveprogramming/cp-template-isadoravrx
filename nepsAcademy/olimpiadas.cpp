a52241ce2f68da763dbf2215da1a65c9707628fa
isadoravrx <isavitoriaxavier@gmail.com>
2023-08-01 15:42:30 -0300

    olimpiadas done

--- nepsAcademy/olimpiadas.cpp
+++ nepsAcademy/olimpiadas.cpp
@@ -2,12 +2,25 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
+vector<vector<int>>medalhas;
+int cont = 0;
+void rep(int i){
+    if(i == 0){
+        return;
+    }else{
+        if(medalhas[i][0] == medalhas[i-1][0] && medalhas[i][1] == medalhas[i-1][1] &&medalhas[i][2] == medalhas[i-1][2]){
+            cont++;
+            return rep(i-1);
+        }else{
+            return;
+        }
+    }
+}
 int main(){
     int paises,modalidades;
     cin >> paises >> modalidades;
     
-    vector<vector<int>>medalhas(paises);
+    medalhas.resize(paises);
     for(int i = 0; i < paises; i++){  //assim, o país 0,1,2... terao suas q de medalhas
         medalhas[i].assign(4,0);//quantidade,depois o numero
         medalhas[i][3] = i + 1;// identidade do pais
@@ -25,12 +38,22 @@ int main(){
     }
 
     sort(medalhas.begin(),medalhas.end());
+    int qprint = 0;
     for(int i = paises - 1; i >= 0; i--){
-        if(i == 0){
-            cout << medalhas[i][3] << endl;
-        }else{
-            cout << medalhas[i][3] << " ";
+        cont = 0;
+        rep(i);
+        
+        for(int j = i - cont; j <= i; j++){
+            if(qprint == paises - 1){
+                cout << medalhas[j][3] << endl;
+                break;
+            }
+
+            cout << medalhas[j][3] << " ";
+            qprint++;
         }
+
+        i = i - cont;
     }
-    
+
 }
