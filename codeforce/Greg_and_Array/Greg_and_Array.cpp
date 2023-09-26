45cfc3c5a96a54bd5ecc79555e9e7fac49c76868
isadoravrx <isavitoriaxavier@gmail.com>
2023-09-26 17:03:13 -0300

    greg and array done, tabela 2 done

--- codeforce/Greg_and_Array/Greg_and_Array.cpp
+++ codeforce/Greg_and_Array/Greg_and_Array.cpp
@@ -1,61 +1,71 @@
 #include <iostream>
 #include <cstring>
-#include <vector>
 using namespace std;
 
 int main(){
-    int n, m, k; 
+    long long n, m, k;
     cin >> n >> m >> k;
-    
-    int array[n];
+
+    long long array[n];
     for(int i = 0; i < n; i++){ // recebendo array
-        int value;
+        long long value;
         cin >> value;
         array[i] = value;
     }
 
-    int fl, fr, somador; //fronteira left...
-    int op[m][n]; //a 4 posicao é pra quantas vezes ele foi utilizado
+    long long fl, fr, somador; //fronteira left...
+    long long op[m][3]; //somador,left,right
+
     memset(op,0,sizeof(op));
+
     for(int i = 0; i < m; i++){ // recebendo as operações e colocando na matriz
         cin >> fl >> fr >> somador;
         fl--;
         fr--;
-        op[i][fl] += somador;
-        if(fr + 1 < n){
-            op[i][fr+1] -= somador;
-        }
+
+        op[i][0] += somador;
+        op[i][1] = fl;
+        op[i][2] = fr;
     }
 
-    int opinicial, opfinal;
-    vector<int>freq_op;
-    freq_op.assign(m,0);
-    for(int i = 0; i < k; i++){ 
+    long long opinicial, opfinal;
+
+    long long timesop[m];
+    memset(timesop,0,sizeof(timesop));
+
+    for(int i = 0; i < k; i++){
         cin >> opinicial >> opfinal;
         opinicial--;
         opfinal--;
-        //talvez isso esteja lento, devo fazer o mesmo da matriz das operacoes?
-        for(int j = opinicial; j <= opfinal; j++){
-            freq_op[j]++;
-        }
-    }  
 
-    vector<long long>cadeia_somadora;
-    cadeia_somadora.assign(n,0);
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            op[i][j] = freq_op[i] * op[i][j];
-            if(j == 0){
-                cadeia_somadora[j] += op[i][j];
-            }else{
-                cadeia_somadora[j] += op[i][j - 1] + op[i][j];
-            }
+        timesop[opinicial]++;
+        if(opfinal + 1 < m){
+            timesop[opfinal + 1]--;
         }
     }
 
+    long long qtimes_op[m]; //guarda a quantidade que cada operacao foi usada
+    long long anterior = 0;
+    for(int i = 0; i < m; i++){
+        qtimes_op[i] = timesop[i] + anterior;
+        anterior = timesop[i] + anterior;
+    }
+
+    long long soma[n];
+    memset(soma,0,sizeof(soma));
+    for(int i = 0; i < m ; i++){
+        op[i][0] = op[i][0] * qtimes_op[i];
+        soma[op[i][1]] += op[i][0];
+        if(op[i][2] + 1 < n){
+            soma[op[i][2] + 1]-=op[i][0];
+        }
+    }
+
+
+    anterior = 0;
     for(int i = 0; i < n; i++){
-        array[i] += cadeia_somadora[i];
-        
+        array[i] += soma[i] + anterior;
+        anterior = soma[i] + anterior;
         if(i == n - 1){
             cout << array[i] << endl;
             break;
