b6859d16abdf961ec706ec61d03cca4d2bec78db
isadoravrx <isavitoriaxavier@gmail.com>
2023-11-21 22:23:57 -0300

    just remembering how to do Brick game - uva

--- codeforce/New_Year_and_Ascent_Sequence/New_Year_and_Ascent_Sequence.cpp
+++ codeforce/New_Year_and_Ascent_Sequence/New_Year_and_Ascent_Sequence.cpp
@@ -1,48 +1,60 @@
 #include <iostream>
 #include <algorithm>
 #include <bitset>
+#include <cstring>
+#include <algorithm>
+#include <vector>
 using namespace std;
 long long n;
-bitset<100000>bt(0); //1 quando é asc
 
 int main(){
     cin >> n;
-    long long lower[n];
-    long long higher[n];
-    for(int i  = 0; i < n; i++){
+    vector<long long> lower;
+    vector<long long> higher;
+
+    long long ans = 0;
+
+    for(long long i  = 0; i < n; i++){
         long long len;
         cin >> len;
+
         long long last;
         long long mx = 0;
         long long mn = 1000000;
-        for(int j = 0; j < len; j++){
-            long long num;
+        bool isascedente = false;
+        long long num;
+
+        bool entrou = false;
+        for(long long j = 0; j < len; j++){
             cin >> num;
             if(j != 0){
                 if(last < num){
-                    bt[i] = 1;
+                    isascedente = true;
+                    if(!entrou){
+                        ans += n + (n -1); //quando se é ascedente, pode formar essa quantidade de pares com eles
+                        entrou = true;
+                    }
                 }
             }
+
             mx = max(mx,num);
             mn= min(mn,num);
             last = num;
         }
-        higher[i] = mx;
-        lower[i] = mn;
-    }
-
-    long long desc = 0;
-    for(int i = 0; i < n; i++){
-        if(bt[i] == 0){ //primeiro desc
-            for(int j = 0; j < n; j++){
-                if(bt[j] == 0){ //segundo desc tbm
-                    if(lower[i] >= higher[j]){ //ver se a combinacao deles tbm é no desc
-                        desc++;
-                    }
-                }
-            }
+        if(!isascedente){
+            higher.push_back(mx);
+            lower.push_back(mn);
         }
+
     }
 
-    cout << (n * n) - desc << endl;
+    sort(lower.begin(),lower.end());
+    sort(higher.begin(),higher.end());
+
+    for(long long i = 0; i < lower.size(); i++){
+            long long ind = (upper_bound(higher.begin(),higher.end(),lower[i]) - higher.begin()); //ponteiro do higher - ponteiro do achado, vai dar o index
+            ans += lower.size() - ind;
+    }
+
+    cout << ans << endl;
 }
