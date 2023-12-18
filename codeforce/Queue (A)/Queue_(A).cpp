b156fea45f47854db138d8ba1ed9d00acff44b98
isadoravrx <isavitoriaxavier@gmail.com>
2023-12-17 21:12:37 -0300

    starting to think about queue

---
+++ codeforce/Queue (A)/Queue_(A).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int k, f1,f2,f3,f4,f5;
+        cin >> k >> f1 >> f2 >> f3 >> f4 >> f5;
+
+    }
+}
