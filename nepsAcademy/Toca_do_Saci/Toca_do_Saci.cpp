1ed1d59a95d4ea90640bbd93a8f0b936b0f65896
isadoravrx <isavitoriaxavier@gmail.com>
2024-01-23 07:54:46 -0300

    trying gincana in another way

---
+++ nepsAcademy/Toca_do_Saci/Toca_do_Saci.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    
+    vector<vector<int>>v;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            int num;
+            cin >> num;
+        }
+    }
+}
