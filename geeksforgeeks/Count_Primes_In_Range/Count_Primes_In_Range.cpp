48cc7f14e2f44bf2c53088070cfd988d379cba75
Isadora Vitória Do Rêgo Xavier <isa@Isadoras-MacBook-Air.local>
2024-02-14 15:43:49 -0300

    cont primes in range done

--- geeksforgeeks/Count_Primes_In_Range/Count_Primes_In_Range.cpp
+++ geeksforgeeks/Count_Primes_In_Range/Count_Primes_In_Range.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
+int v[200002];
 int main(){
-	int l,r;
-	cin >> l >> r;
 	
 	int cont = 0;
-	for(int i = l; i <= r; i++){
-		bool b = true;
-		
-		for(int j = 2; j <= sqrt(i); j++){
-			if(i % j == 0){
-				b = false;
-				break;
-			}
-		}
-	
-		if(b){
-			cont++;	
-		}
-	}
+	for(int i = 2; i <= 200000; i++){
+                bool b = true;
+        
+                for(int j = 2; j <= sqrt(i); j++){
+                    if(i % j == 0){
+                        b = false;
+                        break;
+                    }       
+                }
 
-	cout << cont << endl;
+                if(b && i != 1){  
+                    cont++;
+                }
+        
+                v[i] = cont;
+            
+                
+         }
+
+	
+	int l,r;
+	while(cin >> l >> r){
+		cout << (v[r] - v[l-1]) << endl;
+	}
+            
 }
