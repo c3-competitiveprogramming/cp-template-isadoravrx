5d31e9ca5997f3d03409d59d43e62d2ef61ee5bd
isadoravrx <isadora.00000844511@unicap.br>
2024-02-28 18:44:28 -0300

    t-prime almost done

--- codeforce/T-primes/T-primes.cpp
+++ codeforce/T-primes/T-primes.cpp
@@ -1,38 +1,47 @@
 #include <iostream>
+#include <map>
 #include <cmath>
 using namespace std;
 
 int main(){
+    map<long long,long long>mp;
+    long long aux = 0;
+
+    for(long long i = 1; i <= 1000000; i++){
+        long long num = sqrt(i + aux);
+        double numdoub = sqrt(i+aux);
+        bool isprime = true;
+        
+        for(long long j = 2; j*j <= num; j++){
+            if(num % j == 0){
+                isprime = false;
+                break;
+            }
+        }
+        
+        if(isprime && i != 1 && num == numdoub){
+            mp[i + aux] = 1;
+        }else{
+            mp[i + aux] = 0;
+        }
+        aux++;
+    }
     int n;
     cin >> n;
 
     for(int i = 0; i < n; i++){
         long long num;
         cin >> num;
-       
-        double mynum = num;
-        long long cont = 0;
-        for(int j = 1; j <= sqrt(num); j++){
-            if(cont > 3){
-                break;
-            }
-            if(num % j == 0){
-                cont++;
-            }
-        }
-
-        double half = mynum /2;
-        int myhalf = num/2;
-        
-        cont++;
-        if(half == myhalf && sqrt(num) < myhalf){
-            cont++;
-        }
-
-        if(cont == 3){
+        if(num == 4){
             cout << "YES" << endl;
-        }else{
+        }else if(num % 2 == 0){
             cout << "NO" << endl;
+        }else{
+            if(mp[num] == 1){
+                cout << "YES" << endl;
+            }else{
+                cout << "NO" << endl;
+            }
         }
     }
 }
