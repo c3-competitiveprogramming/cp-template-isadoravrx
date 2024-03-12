2447d7b6f3b6793fe3707d31fdb3cebf713ec5d6
Isadora Vitória Do Rêgo Xavier <isa@Isadoras-MacBook-Air.local>
2024-03-12 16:10:08 -0300

    Euler Totient function

--- spoj/Euler_Totient_Function/Euler_Totient_Function_2.cpp
+++ spoj/Euler_Totient_Function/Euler_Totient_Function_2.cpp
@@ -2,20 +2,20 @@
 #include <cmath>
 using namespace std;
 
-long long phi(double n){
+long long phi(int n){
     double val = n;
-    for(long long i = 2; i <= sqrt(n); i++){
+    for(int i = 2; i <= sqrt(n); i++){
         if(n % i == 0){
             while(n %i == 0){
-                n /= i;
+                n = ((double)n / (double)i);
             }
 
-            val *= (1 - 1/i);
+            val *= (1.0 - (1.0/(double)i));
         }
     }
 
     if(n > 1){
-        val -= val/n;
+        val = val - val/ (double)n;
     }
 
     return val;
