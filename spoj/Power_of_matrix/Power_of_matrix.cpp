134269b873016d43d3d3ae5e9dbbdc42b5d5a156
isadoravrx <isavitoriaxavier@gmail.com>
2024-03-26 16:40:20 -0300

    Power of matrix done

--- spoj/Power_of_matrix/Power_of_matrix.cpp
+++ spoj/Power_of_matrix/Power_of_matrix.cpp
@@ -2,41 +2,64 @@
 #include <vector>
 using namespace std;
 
-int main(){
-    int t;
+long long m,n;
+vector<vector<long long> > mult(vector<vector<long long> > &a,vector<vector<long long> > &b){
+    vector<vector<long long> >ans(m,vector<long long>(m));
+    for(long long i = 0; i < m; i++){
+        for(long long j = 0; j < m; j++){
+           long long sum = 0;
+           for(long long k = 0; k < m; k++){
+               sum += ((a[i][k] % 1000000007) * (b[k][j] % 1000000007)) % 1000000007;
+           }
+
+           ans[i][j] = sum % 1000000007;
+        } 
+    }
+
+    return ans;
+}
+
+int  main(){
+    long long t;
     cin >> t;
 
     while(t--){
-        int m,n;
         cin >> m >> n;
-
-        vector<vector<int> >ans(m,vector<int>(m));
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < m; j++){
-                int num;
+        
+        vector<vector<long long> >mat(m,vector<long long>(m));
+        vector<vector<long long> >ans(m,vector<long long>(m));
+        for(long long i = 0; i < m; i++){
+            for(long long j = 0; j < m; j++){
+                long long num;
                 cin >> num;
-                ans[i][j] = num;
+                mat[i][j] = num;   
+                if(i == j){
+                    ans[i][j] = 1;
+                }
             }
         }
 
-        while(n--){
-            for(int i = 0; i < m; i++){
-                for(int j = 0; j < m; j++){
-                    for(int k = 0; k < m; k++){
-                        ans[i][j] += ((ans[i][k] % 1000000007) * (ans[k][j] % 1000000007)) % 1000000007;
-                    }
-                } 
-                if(n == 0 && i == m-1){
-                    cout << ans[i][j];
-                    if(j == m-1){
-                        cout << endl;
-                    }else{
-                        cout << " ";
-                    }
-                }
-
+    
+        while(n != 0){
+            if(n % 2 == 0){
+                mat = mult(mat,mat);
+                n/=2;
+            }else{
+                ans = mult(ans,mat);
+                n--;
             }
-        } 
+        }
+
+        for(long long i = 0; i < m; i++){
+            for(long long j = 0; j < m; j++){
+                if(j == m-1){
+                    cout << ans[i][j] << endl;
+                }else{
+                    cout << ans[i][j] << " ";
+                }
+            }
+        }
+
 
     }
 }
