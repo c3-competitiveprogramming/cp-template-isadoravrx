711872c9e1072fbcb2c127d8f34649cb5615d0b6
isadoravrx <isavitoriaxavier@gmail.com>
2024-05-11 16:33:11 -0300

    simple strings done

--- codeforce/Simple_Strings/Simple_Strings.cpp
+++ codeforce/Simple_Strings/Simple_Strings.cpp
@@ -10,58 +10,50 @@ int main(){
     int i = 0;
 
     while(i < s.size()){
-        if(i!= s.size()-1 && s[i] == s[i+1]){
+        if(i != s.size()  && s[i] == s[i+1]){
+
             int ini = i;
             int fim = i;
 
-            int cont = 1;
-            for(int j = 0; j < s.size(); j++){
-                if(s[j] != s[ini]){
+            for(int j = i+1; j < s.size(); j++){
+                if(s[i] == s[j]){
+                    fim++;
+                }else{
                     break;
                 }
-                fim++;
-                cont++;
             }
-            
-            if(cont % 2 == 0){
-                for(int j =  ini; j <= fim; j+=2){
-                     if(j >= 0 && s[j-1] != 'c' && s[j+1] != 'c'){
-                        s[j] = 'c';
-                    }else if(j >= 0 && s[j-1] != 'a' && s[j+1] != 'a'){
-                         s[j] = 'a';
-                    }else if(j >= 0 && s[j-1] != 'b' && s[j+1] != 'b'){
-                         s[j] = 'b';
-                    }else if(j == 0){
-                        if(s[j + 1] != 'a'){
-                            s[j] = 'a';
-                        }else{
-                            s[j] = 'b';
-                        }
-                    }
 
+            for(int j = ini+1; j <= fim; j+=2){
+                if(j == 0 && j+1 < s.size()){
+                    if(s[j+1] != 'a'){
+                        s[j] = 'a';
+                    }else{
+                        s[j] = 'b';
+                    }
+                }else if(j == s.size()-1){
+                    if(s[j-1] != 'a'){
+                        s[j] = 'a';
+                    }else{
+                        s[j] = 'b';
+                    }
+                }else{
+                    if(s[j+1] != 'a' && s[j-1] != 'a'){
+                        s[j] = 'a';
+                    }else if(s[j+1] != 'b' && s[j-1] != 'b'){
+                            s[j] = 'b';
+                    }else{
+                            s[j] = 'c';
+                    }
                 }
-            }else{
-                for(int j =  ini + 1; j <= fim; j+=2){
-                    if(j >= 0 && s[j-1] != 'c' && s[j+1] != 'c'){
-                        s[j] = 'c';
-                    }else if(j >= 0 && s[j-1] != 'a' && s[j+1] != 'a'){
-                         s[j] = 'a';
-                    }else if(j >= 0 && s[j-1] != 'b' && s[j+1] != 'b'){
-                         s[j] = 'b';
-                    }else if(j == 0){
-                        if(s[j + 1] != 'a'){
-                            s[j] = 'a';
-                        }else{
-                            s[j] = 'b';
-                        }
-                    }
-                } 
             }
 
-            
             i = fim + 1;
+        }else{
+            i++;
         }
     }
+                            
     cout << s << endl;
+                            
 }
 
