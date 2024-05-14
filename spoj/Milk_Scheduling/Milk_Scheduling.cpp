07aa32155e2ea66f182a224a971e7961bd40fe2d
isadoravrx <isavitoriaxavier@gmail.com>
2024-05-14 17:58:37 -0300

    Milk_Scheduling e Mission_in_Amman_A done

--- spoj/Milk_Scheduling/Milk_Scheduling.cpp
+++ spoj/Milk_Scheduling/Milk_Scheduling.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
-#include <map>
+#include <vector>
+#include <set>
+
 using namespace std;
 
 int main(){
     int n;
     cin >> n;
-    
-    map<int,int>mp;
-    
+
+
     int l,t;
+    int mx = 0;
+    multiset<pair<int,int> >set;
+
     for(int i = 0; i < n; i++){
         cin >> l >> t;
-        
-        if(mp.find(t) != mp.end() && mp[t] > l){
-            continue;
-        }
-        mp[t] = l;
+        set.insert(make_pair(l,t));
+        mx = max(mx,t);
     }
 
-        
-    int time = 0;
-    int cont = 0;
-    for(auto it : mp){
-        if(it.second >= time){
-            cont += it.second;
+    vector<int>v(mx+1); 
+    int ans = 0;
+
+    multiset<pair<int,int> >::reverse_iterator rit;
+
+    for (rit = set.rbegin(); rit != set.rend(); rit++){
+        if(v[(*rit).second] == 0){
+            ans += (*rit).first;
+            v[(*rit).second] = 1;
+        }else{
+            for(int i = (*rit).second -1; i > 0; i--){
+                if(v[i] == 0){
+                    ans += (*rit).first;
+                    v[i] = 1;
+                    break;
+                }    
+            }
         }
-
-        time++;
     }
-
-    cout << cont << endl;
-
+    cout << ans << endl;
 }
